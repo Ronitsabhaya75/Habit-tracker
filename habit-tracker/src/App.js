94d6209675e0380223cca2a67f457766e7ed5700
Pagilla, Siddartha Reddy <siddarthareddypagilla@my.unt.edu>
2025-03-03 22:09:33 +0000

    Update habit-tracker/src/App.js

--- habit-tracker/src/App.js
+++ habit-tracker/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';  // Correct path
+import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
-
+import BreakthroughGame from './components/BreakthroughGame'; // New import
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/breakthrough-game" element={<BreakthroughGame />} /> {/* New Route */}
       </Routes>
     </AuthProvider>
   );
