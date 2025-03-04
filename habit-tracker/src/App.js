1d4ed2000887ac889a0166e0f571304e8146d1e7
Pagilla, Siddartha Reddy <siddarthareddypagilla@my.unt.edu>
2025-03-04 00:24:23 +0000

    Update App.js

--- habit-tracker/src/App.js
+++ habit-tracker/src/App.js
@@ -1,35 +1,28 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import { ThemeProvider as CustomThemeProvider } from './context/ThemeContext';
 import { AuthProvider } from './context/AuthContext';
 import { HabitProvider } from './context/HabitContext';
-import { theme } from './theme';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
-import BreakthroughGame from './components/BreakthroughGame';
+import BreakthroughGame from './components/BreakthroughGame'; // New import
 
 function App() {
   return (
-    <CustomThemeProvider>
-      <ThemeProvider theme={theme}>
-        <AuthProvider>
-          <HabitProvider>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/breakthrough-game" element={<BreakthroughGame />} />
-            </Routes>
-          </HabitProvider>
-        </AuthProvider>
-      </ThemeProvider>
-    </CustomThemeProvider>
+    <AuthProvider>
+      <HabitProvider>
+        <Navbar /> {/* Navbar component */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/breakthrough-game" element={<BreakthroughGame />} /> {/* New Route */}
+        </Routes>
+      </HabitProvider>
+    </AuthProvider>
   );
 }
 
