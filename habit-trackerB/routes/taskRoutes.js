56d7a4e6aeebe540b70ad5539f3c0dc7bdc73dab
Sakthivel Pandi, Shivam <shivamsakthivelpandi@my.unt.edu>
2025-03-24 04:46:07 +0000

    Update taskRoutes.js

--- habit-trackerB/routes/taskRoutes.js
+++ habit-trackerB/routes/taskRoutes.js
@@ -4,7 +4,7 @@ const { verifyToken } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-// Get all tasks
+// Get all tasks for a user
 router.get('/', verifyToken, async (req, res) => {
   const tasks = await Task.find({ userId: req.user.id });
   res.json({ tasks });
@@ -12,17 +12,26 @@ router.get('/', verifyToken, async (req, res) => {
 
 // Create a new task
 router.post('/', verifyToken, async (req, res) => {
-  try {
-    const { name } = req.body;
-    if (!name) return res.status(400).json({ message: 'Task name is required' });
+  const { name } = req.body;
+  if (!name) return res.status(400).json({ message: 'Task name is required' });
 
-    const newTask = new Task({ userId: req.user.id, name });
-    await newTask.save();
+  const newTask = new Task({ userId: req.user.id, name });
+  await newTask.save();
 
-    res.status(201).json({ message: 'Task created', task: newTask });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  res.status(201).json({ message: 'Task created', task: newTask });
+});
+
+// Mark task as completed
+router.put('/:taskId', verifyToken, async (req, res) => {
+  const { completed } = req.body;
+  const task = await Task.findByIdAndUpdate(req.params.taskId, { completed }, { new: true });
+  res.json({ message: 'Task updated', task });
+});
+
+// Delete a task
+router.delete('/:taskId', verifyToken, async (req, res) => {
+  await Task.findByIdAndDelete(req.params.taskId);
+  res.json({ message: 'Task deleted' });
 });
 
 module.exports = router;
