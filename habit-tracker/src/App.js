6ecffba5402c31c5bb9d4abd1c8cbfb3e4a9d825
Aashishpal Reddy Kandala <aashishpalreddykandala@my.unt.edu>
2025-03-26 00:52:37 -0500

    Updated the App.js by including the code lines required for HabitQuiz.js

--- habit-tracker/src/App.js
+++ habit-tracker/src/App.js
@@ -1,48 +1,50 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import { HabitProvider } from './context/HabitContext';
-import { EventProvider } from './context/EventContext';
-import Dashboard from './components/Dashboard';
-import BreakthroughGame from './components/BreakthroughGame';
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/Home';
-import Track from './components/Track';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './theme';
-import AddictionRecoveryGame from './components/games/AddictionRecoveryGame';
-import DashboardReview from './components/DashboardReview';
-import SpinWheel from './components/SpinWheel';
-import HabitProgressTracker from './components/HabitProgressTracker';
-import Chess from './components/games/chess';
-import LudoGame from './components/games/ludo/LudoGame'; // Add this import
+  import React from 'react';
+  import { Routes, Route } from 'react-router-dom';
+  import { AuthProvider } from './context/AuthContext';
+  import { HabitProvider } from './context/HabitContext';
+  import { EventProvider } from './context/EventContext';
+  import Dashboard from './components/Dashboard';
+  import BreakthroughGame from './components/BreakthroughGame';
+  import Login from './components/Login';
+  import Register from './components/Register';
+  import Home from './components/Home';
+  import Track from './components/Track';
+  import { ThemeProvider } from 'styled-components';
+  import { theme } from './theme';
+  import AddictionRecoveryGame from './components/games/AddictionRecoveryGame';
+  import DashboardReview from './components/DashboardReview';
+  import SpinWheel from './components/SpinWheel';
+  import HabitProgressTracker from './components/HabitProgressTracker';
+  import Chess from './components/games/chess';
+  import LudoGame from './components/games/ludo/LudoGame'; // Add this import
+  import HabitQuiz from './components/HabitQuiz';
 
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <AuthProvider>
-        <HabitProvider>
-          <EventProvider>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/spinWheel" element={<SpinWheel />} />
-              <Route path="/habitProgressTracker" element={<HabitProgressTracker />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/track" element={<Track />} />
-              <Route path="/review" element={<DashboardReview />} />
-              <Route path="/breakthrough-game" element={<BreakthroughGame />} />
-              <Route path="/addiction-recovery" element={<AddictionRecoveryGame />} />
-              <Route path="/chess" element={<Chess />} />
-              <Route path="/ludo" element={<LudoGame />} /> {/* Add this route */}
-            </Routes>
-          </EventProvider>
-        </HabitProvider>
-      </AuthProvider>
-    </ThemeProvider>
-  );
-}
+  function App() {
+    return (
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <HabitProvider>
+            <EventProvider>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/spinWheel" element={<SpinWheel />} />
+                <Route path="/habitQuiz" element={<HabitQuiz />} />
+                <Route path="/habitProgressTracker" element={<HabitProgressTracker />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/track" element={<Track />} />
+                <Route path="/review" element={<DashboardReview />} />
+                <Route path="/breakthrough-game" element={<BreakthroughGame />} />
+                <Route path="/addiction-recovery" element={<AddictionRecoveryGame />} />
+                <Route path="/chess" element={<Chess />} />
+                <Route path="/ludo" element={<LudoGame />} /> {/* Add this route */}
+              </Routes>
+            </EventProvider>
+          </HabitProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    );
+  }
 
-export default App;
+  export default App;
