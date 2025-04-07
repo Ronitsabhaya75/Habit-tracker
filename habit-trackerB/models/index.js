9ee0353ef7553bc5d96db28a87a233cea7f87406
Ronitkumar Sabhaya <RonitkumarSabhaya@my.unt.edu>
2025-04-07 15:57:11 -0500

    updated the models

--- habit-trackerB/models/index.js
+++
@@ -1,6 +0,0 @@
-import User from './User.js';
-import HabitSchema from './habit.js';
-import Task from './Task.js';
-import LocalData from './LocalData.js';
-
-export { User, HabitSchema, Task, LocalData };
